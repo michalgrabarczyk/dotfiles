302d032333ca2e06caac09110e5af2da24fa10fb
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2019-09-24 10:54:56 +0200

    add use_dotfiles bash script

---
+++ use_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+read -p "This script will replace all of your dotfiles. Continue? [y/n] " -n 1 -r
+echo # move to the new line
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Creating symlinks..."
+
+    ln -sf "$SCRIPT_DIR"/.bashrc ~/.bashrc ; echo ".bashrc symlink created"
+    ln -sf "$SCRIPT_DIR"/.gitconfig ~/.gitconfig ; echo ".gitconfig symlink created"
+    ln -sf "$SCRIPT_DIR"/.gitignore ~/.gitignore ; echo ".gitignore symlink created"
+    
+    echo "Creating symlinks done!"
+fi
+
